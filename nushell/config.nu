fa2b10ac79b72fc0c0427bed72fce7a8274438e1
glassesneo <glassesneo@protonmail.com>
2025-02-25 23:42:01 +0900

    update

--- nushell/config.nu
+++ nushell/config.nu
@@ -1,6 +1,5 @@
 # $env.config.edit_mode = "vi"
 $env.config.show_banner = false
-$env.config.rm.always_trash = true
 $env.config.buffer_editor = "nvim"
 
 # aliases
--- sketchybar/items/cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "$CONFIG_DIR/colors.sh"
-
-sketchybar \
-    --add item cpu right \
-    --set cpu update_freq=2 script="$PLUGIN_DIR/cpu.sh" background.color="$CPU_BACKGROUND" label.color="$CPU"
-
