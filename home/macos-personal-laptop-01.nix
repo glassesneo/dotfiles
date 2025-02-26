2e90a5172a725add7e5d2d9459aa9a46e9a5fb22
glassesneo <glassesneo@protonmail.com>
2025-02-26 10:28:10 +0900

    update

--- home/macos-personal-laptop-01.nix
+++ home/macos-personal-laptop-01.nix
@@ -1,10 +1,34 @@
 {pkgs, ...}: {
-  home.stateVersion = "24.11";
+  home = {
+    username = builtins.getEnv "USER";
+    homeDirectory = builtins.getEnv "HOME";
+    stateVersion = "24.11";
+    packages = with pkgs; [
+      bat
+      btop
+      coreutils
+      devbox
+      duf
+      fastfetch
+      fd
+      fzf
+      jq
+      nowplaying-cli
+      ripgrep
+      sl
+      unrar
+      uv
+      vim-startuptime
+      xz
+    ];
+  };
+
+  programs.home-manager.enable = true;
   imports = [
     ./common/direnv.nix
     ./common/git.nix
     ./common/gh.nix
-    ./common/eza.nix
+    # ./common/eza.nix
     ./common/neovim.nix
     ./common/oh-my-posh.nix
     # ./common/starship.nix
@@ -13,8 +37,5 @@
     ./darwin/homebrew.nix
     ./darwin/services.nix
     ./darwin/gui.nix
-  ];
-  home.packages = with pkgs; [
-    nowplaying-cli
   ];
 }
--- home/ubuntu-dev-vm-01.nix
+++ home/ubuntu-dev-vm-01.nix
@@ -1,5 +1,27 @@
-{
-  home.stateVersion = "25.05";
+{pkgs, ...}: {
+  home = {
+    username = builtins.getEnv "USER";
+    homeDirectory = builtins.getEnv "HOME";
+    stateVersion = "25.05";
+    packages = with pkgs; [
+      bat
+      btop
+      coreutils
+      duf
+      fastfetch
+      fd
+      fzf
+      jq
+      ripgrep
+      sl
+      unrar
+      uv
+      vim-startuptime
+      xz
+    ];
+  };
+
+  programs.home-manager.enable = true;
   imports = [
     ./common/direnv.nix
     ./common/git.nix
