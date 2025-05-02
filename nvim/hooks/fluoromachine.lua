f6135df6f59074d170f12a293be0c037bdd21aec
glassesneo <glassesneo@protonmail.com>
2025-05-02 16:24:13 +0900

    remove old nvim config

--- nvim/hooks/fluoromachine.lua
+++
@@ -1,9 +0,0 @@
---- lua_source {{{
-local fm = require("fluoromachine")
-fm.setup({
-  glow = false,
-  theme = "retrowave",
-  transparent = true,
-})
-vim.cmd([[ colorscheme fluoromachine ]])
---- }}}
