0cce6a1d1a9fda438c5becebd14c8cc4668ecec3
glassesneo <s1f102500034@iniad.org>
2025-08-09 16:32:55 +0900

    add git

---
+++ modules/programs/git.nix
@@ -0,0 +1,21 @@
+{delib, ...}:
+delib.module {
+  name = "programs.git";
+
+  options.programs.git = with delib; {
+    enable = boolOption true;
+    enableLFS = boolOption true;
+  };
+
+  home.ifEnabled.programs.git = {
+    myconfig,
+    cfg,
+    ...
+  }: {
+    enable = cfg.enable;
+    lfs.enable = cfg.enableLFS;
+
+    userName = myconfig.constants.username;
+    userEmail = myconfig.constants.useremail;
+  };
+}
