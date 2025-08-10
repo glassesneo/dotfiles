74d47533b80777873a2a903ca50b6d55455dd488
neo <glassesneo@protonmail.com>
2025-08-10 18:05:21 +0900

    add toggleterm

---
+++ modules/programs/nixvim/plugins/toggleterm.nix
@@ -0,0 +1,23 @@
+{delib, ...}:
+delib.module {
+  name = "programs.nixvim.plugins.toggleterm";
+
+  options = delib.singleEnableOption true;
+
+  home.ifEnabled.programs.nixvim = {
+    plugins = {
+      toggleterm = {
+        enable = true;
+        settings = {
+          direction = "float";
+          float_opts = {
+            border = "curved";
+            height = 30;
+            width = 130;
+          };
+          open_mapping = "[[<Space>\\]]";
+        };
+      };
+    };
+  };
+}
