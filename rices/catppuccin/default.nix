811925512080347b0e71f14ba1e7c2ba9e0d3da3
neo <glassesneo@protonmail.com>
2026-02-25 15:22:04 +0900

    fix(neovim): make background transparent in catppuccin

--- rices/catppuccin/default.nix
+++ rices/catppuccin/default.nix
@@ -69,6 +69,40 @@ in
               };
             };
           };
+          autoCmd = [
+            {
+              event = ["ColorScheme" "VimEnter"];
+              pattern = ["*"];
+              once = false;
+              callback.__raw = ''
+                function()
+                  local groups = {
+                    "Normal", "NormalNC", "NormalFloat",
+                    "SignColumn", "EndOfBuffer", "LineNr", "CursorLineNr",
+                    "Folded", "FoldColumn", "VertSplit", "StatusLine", "StatusLineNC",
+                  }
+                  for _, g in ipairs(groups) do
+                    vim.api.nvim_set_hl(0, g, { bg = "none" })
+                  end
+                end
+              '';
+            }
+          ];
+          highlight = {
+            # Transparent background guards - override catppuccin plugin post-load
+            # Normal.bg = "none";
+            # NormalNC.bg = "none";
+            # NormalFloat.bg = "none";
+            # SignColumn.bg = "none";
+            # EndOfBuffer.bg = "none";
+            # LineNr.bg = "none";
+            # CursorLineNr.bg = "none";
+            # Folded.bg = "none";
+            # FoldColumn.bg = "none";
+            # VertSplit.bg = "none";
+            # StatusLine.bg = "none";
+            # StatusLineNC.bg = "none";
+          };
         };
       };
     };
