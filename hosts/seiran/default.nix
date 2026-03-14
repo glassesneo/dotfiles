82f213102157f50cf87ac4cc171b20011c581903
neo <glassesneo@protonmail.com>
2026-03-14 20:09:17 +0900

    feat(host): add seiran host

---
+++ hosts/seiran/default.nix
@@ -0,0 +1,7 @@
+{delib, ...}:
+delib.host {
+  name = "seiran";
+  type = "laptop";
+  rice = "catppuccin";
+  tier = "full";
+}
---
+++ hosts/seiran/hardware.nix
@@ -0,0 +1,12 @@
+{delib, ...}:
+delib.host {
+  name = "seiran";
+
+  homeManagerSystem = "aarch64-darwin";
+  home.home.stateVersion = "25.05";
+
+  darwin = {
+    nixpkgs.hostPlatform = "aarch64-darwin";
+    system.stateVersion = 4;
+  };
+}
