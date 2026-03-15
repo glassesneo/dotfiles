42cbb2a686504eba6a2dcb8b68a55d5e840b07f3
neo <glassesneo@protonmail.com>
2026-03-15 16:40:19 +0900

    feat(ssh): abstract remote login between hosts

--- modules/programs/ssh/default.nix
+++ modules/programs/ssh/default.nix
@@ -1,5 +1,6 @@
 {
   delib,
+  host,
   lib,
   pkgs,
   ...
@@ -12,36 +13,49 @@ delib.module {
     mainIdentity = readOnly (strOption "~/.ssh/id_ed25519_personal");
   };
 
-  home.ifEnabled = {cfg, ...}: {
-    programs.ssh = {
+  home.ifEnabled = {
+    cfg,
+    myconfig,
+    ...
+  }: {
+    programs.ssh = let
+      otherHosts = lib.filter (h: h != host.name) (builtins.attrNames myconfig.hosts);
+    in {
       enable = true;
       enableDefaultConfig = false;
 
-      matchBlocks = {
-        "*" = {
-          extraOptions = lib.mkMerge [
-            {
-              AddKeysToAgent = "yes";
-              IdentitiesOnly = "yes";
-              SetEnv = "TERM=xterm-256color";
-            }
-            (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-              UseKeychain = "yes";
-            })
-          ];
-        };
-        "github.com" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = cfg.mainIdentity;
-        };
+      matchBlocks = lib.mkMerge [
+        {
+          "*" = {
+            extraOptions = lib.mkMerge [
+              {
+                AddKeysToAgent = "yes";
+                IdentitiesOnly = "yes";
+                SetEnv = "TERM=xterm-256color";
+              }
+              (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
+                UseKeychain = "yes";
+              })
+            ];
+          };
+          "github.com" = {
+            hostname = "github.com";
+            user = "git";
+            identityFile = cfg.mainIdentity;
+          };
 
-        "github-company" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = "~/.ssh/id_ed25519_company";
-        };
-      };
+          "github-company" = {
+            hostname = "github.com";
+            user = "git";
+            identityFile = "~/.ssh/id_ed25519_company";
+          };
+        }
+        (lib.genAttrs otherHosts (host: {
+          hostname = "${host}.local";
+          user = myconfig.constants.username;
+          identityFile = cfg.mainIdentity;
+        }))
+      ];
     };
   };
 }
