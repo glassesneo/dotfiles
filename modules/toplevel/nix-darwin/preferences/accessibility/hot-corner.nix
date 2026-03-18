ebd471abcfa57ca478c70a87bf67323946e7601e
neo <glassesneo@protonmail.com>
2026-03-18 21:48:54 +0900

    feat(nix-darwin): add hot corner configuration for accessibility preferences

---
+++ modules/toplevel/nix-darwin/preferences/accessibility/hot-corner.nix
@@ -0,0 +1,16 @@
+{delib, ...}:
+delib.module {
+  name = "nix-darwin.preferences.accessibility.hot-corner";
+
+  options = delib.singleEnableOption true;
+
+  darwin.ifEnabled = {
+    system.defaults.dock = {
+      # 1 = Disabled
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
+      wvous-bl-corner = 13; # Lock Screen
+      wvous-br-corner = 1;
+    };
+  };
+}
