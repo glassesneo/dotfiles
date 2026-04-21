9710f0d768cb2b1afa34ba83429c20eb9771bebf
neo <glassesneo@protonmail.com>
2026-04-21 17:05:10 +0900

    feat(pay-respects): disable pay-respects and run sl

--- modules/programs/pay-respects.nix
+++ modules/programs/pay-respects.nix
@@ -1,14 +1,29 @@
-{delib, ...}:
+{
+  delib,
+  lib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "programs.pay-respects";
 
   options.programs.pay-respects = with delib; {
     enable = boolOption true;
+    useSl = boolOption true;
   };
 
   home.ifEnabled = {cfg, ...}: {
     programs.pay-respects = {
-      enable = cfg.enable;
+      enable = cfg.enable && !cfg.useSl;
     };
+
+    home.packages = lib.mkIf cfg.useSl [pkgs.sl];
+
+    programs.zsh.initContent = lib.mkIf cfg.useSl (lib.mkAfter ''
+      command_not_found_handler() {
+        "${lib.getExe pkgs.sl}"
+        return 127
+      }
+    '');
   };
 }
