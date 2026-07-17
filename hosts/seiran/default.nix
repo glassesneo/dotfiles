57672a7dccd84550a3ac63dc41c0939fdaecb486
neo <glassesneo@protonmail.com>
2026-07-17 17:29:20 +0900

    feat: add an abstraction layer for tart vm

--- hosts/seiran/default.nix
+++ hosts/seiran/default.nix
@@ -1,7 +1,6 @@
 {
   delib,
   lib,
-  pkgs,
   ...
 }:
 delib.host {
@@ -13,27 +12,9 @@ delib.host {
   myconfig.services.kanata.profile = "macbook-us";
   myconfig.darwin.window-manager.backend = lib.mkForce "rift";
   myconfig.programs.appcleaner.enable = true;
-  myconfig.user.uid = 501;
-
-  home = {myconfig, ...}: {
-    programs.ssh = {
-      settings = {
-        "seiran-vm1" = let
-          tart = lib.getExe myconfig.programs.tart.package;
-          socat = lib.getExe pkgs.socat;
-          seiranVm1Proxy = pkgs.writeShellScript "seiran-vm1-ssh-proxy" ''
-            ip="$(${tart} ip seiran-vm1)"
-            exec ${socat} STDIO "TCP-CONNECT:$ip:$1"
-          '';
-        in {
-          User = "neo";
-          IdentityFile = myconfig.programs.ssh.mainIdentity;
-          IdentitiesOnly = true;
-          ForwardAgent = true;
-
-          ProxyCommand = "${seiranVm1Proxy} %p";
-        };
-      };
-    };
+  myconfig.programs.tart.vms = {
+    seiran-vm0.os = "linux";
+    seiran-vm1.os = "darwin";
   };
+  myconfig.user.uid = 501;
 }
--- modules/programs/tart/default.nix
+++ modules/programs/tart/default.nix
@@ -1,6 +1,7 @@
 {
   delib,
   host,
+  lib,
   pkgs,
   tiers,
   ...
@@ -9,12 +10,35 @@ delib.module {
   name = "programs.tart";
 
   options = with delib;
-    moduleOptions {
+    moduleOptions ({myconfig, ...}: {
       enable = boolOption (pkgs.stdenv.isDarwin && tiers.atLeast host.tier "standard");
       package = packageOption pkgs.tart;
-    };
+      vms = attrsOfOption (submodule {
+        options = {
+          os = noDefault (enumOption ["linux" "darwin"] null);
+          sshUser = strOption myconfig.constants.username;
+          identityFile = strOption myconfig.programs.ssh.mainIdentity;
+        };
+      }) {};
+    });
 
-  home.ifEnabled = {cfg, ...}: {
+  home.ifEnabled = {cfg, ...}: let
+    mkSshProxy = vmName:
+      pkgs.writeShellScript "${vmName}-ssh-proxy" ''
+        ip="$(${lib.getExe cfg.package} ip ${lib.escapeShellArg vmName})"
+        exec ${lib.getExe pkgs.socat} STDIO "TCP-CONNECT:$ip:$1"
+      '';
+  in {
     home.packages = [cfg.package];
+
+    programs.ssh.settings =
+      lib.mapAttrs (vmName: vm: {
+        User = vm.sshUser;
+        IdentityFile = vm.identityFile;
+        IdentitiesOnly = true;
+        ForwardAgent = true;
+        ProxyCommand = "${mkSshProxy vmName} %p";
+      })
+      cfg.vms;
   };
 }
