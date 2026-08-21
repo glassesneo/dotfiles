3af3d95e35f298166e658034f57a545ad5d6b302
neo <glassesneo@protonmail.com>
2026-08-22 01:16:55 +0900

    feat(discord): add disableBreakingUpdates activation script to home-manager

--- modules/programs/discord.nix
+++ modules/programs/discord.nix
@@ -1,19 +1,30 @@
 {
   brewCasks,
   delib,
+  homeConfig,
   host,
+  pkgs,
   tiers,
   ...
 }:
 delib.module {
   name = "programs.discord";
 
-  options = delib.singleEnableOption (host.guiShellFeatured && tiers.atLeast host.tier "standard");
+  options = with delib;
+    moduleOptions {
+      enable = boolOption (pkgs.stdenv.isDarwin && tiers.atLeast host.tier "standard");
+      package = packageOption brewCasks.discord;
+    };
 
   home.ifEnabled = {
     programs.discord = {
       enable = true;
       package = brewCasks.discord;
     };
+
+    # disableBreakingUpdates does not exist in the brew cask and does not interfere with the package itself, so we can use `pkgs.discord`
+    home.activation.disableDiscordUpdates = homeConfig.lib.dag.entryAfter ["writeBoundary"] ''
+      ${pkgs.discord.disableBreakingUpdates}/bin/disable-breaking-updates.py
+    '';
   };
 }
